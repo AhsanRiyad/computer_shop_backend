60038f981d78a4ac141fff0e39d78723bfe56f92
Riyad Ahsan <riyad298@gmail.com>
2020-11-02 03:01:55 +0600

    modified for namecheap

--- app/Models/Orders/Order.php
+++ app/Models/Orders/Order.php
@@ -1,144 +1,297 @@
 <?php
 
+
+
 namespace App\Models\Orders;
 
+
+
 use Illuminate\Database\Eloquent\Model;
+
 use Illuminate\Support\Facades\DB;
+
 class Order extends Model
+
 {
+
     //
+
     protected $fillable  = ['id' , 'status' , 'type' , 'correction_status' , 'reference' , 'date' , 'notes' , 'discount' , 'client_id'];
 
+
+
     public function address()
+
     {
+
         return $this->hasOne('App\Models\Addresses\Address', 'order_id');
+
     }
+
+
 
     public function created_by()
+
     {
+
         return $this->belongsTo('App\User', 'created_by', 'id');
+
     }
+
+
 
     public function updated_by()
+
     {
+
         return $this->belongsTo('App\User', 'created_by', 'id');
+
     }
+
+
 
     public function client()
+
     {
+
         return $this->belongsTo('App\Models\Clients\Client', 'client_id' , 'id');
+
     }
 
+
+
     public function warranty()
+
     {
+
         return $this->hasMany('App\Models\Warranties\Warranty', 'order_id');
+
     }
+
+
+
 
 
     public function order_details()
+
     {
+
         return $this->hasMany('App\Models\Orders\Order_detail', 'order_id')->with(['products' , 'serial_numbers_purchase', 'serial_numbers_sell']);
+
     }
+
+
+
 
 
     public function serial_numbers()
+
     {
+
         /*return $this->hasManyThrough('App\Models\Products\Serial_number', 'App\Models\Orders\Order_detail')->with(['product']);*/
+
+
 
         return $this->hasManyThrough('App\Models\Products\Serial_number', 'App\Models\Orders\Order_detail');
+
     }
+
+
 
     public function serial_numbers_purchase()
+
     {
+
         /*return $this->hasManyThrough('App\Models\Products\Serial_number', 'App\Models\Orders\Order_detail')->with(['product']);*/
 
+
+
         return $this->hasManyThrough('App\Models\Products\Serial_purchase', 'App\Models\Orders\Order_detail', 'order_id' , 'order_detail_id', 'id' , 'id');
+
     }
+
+
+
 
 
     public function serial_numbers_sell()
+
     {
+
         /*return $this->hasManyThrough('App\Models\Products\Serial_number', 'App\Models\Orders\Order_detail')->with(['product']);*/
 
+
+
         return $this->hasManyThrough('App\Models\Products\Serial_sell', 'App\Models\Orders\Order_detail', 'order_id' , 'order_detail_id', 'id' , 'id');
+
     }
+
+
 
     public function products()
+
     {
+
         return $this->hasManyThrough('App\Models\Products\Product', 'App\Models\Orders\Order_detail');
+
     }
 
+
+
     public function transactions()
+
     {
+
         return $this->hasMany('App\Models\Transactions\Transaction', 'order_id')->with(['client']);
+
     }
-	
+
+
+
     public function order_return()
+
     {
+
         return $this->hasMany('App\Models\Orders\Order_return', 'order_id');
+
     }
+
+
+
 
 
     public function getSubTotal()
+
     {
+
         return $this->order_details()->sum(DB::raw('quantity * price'));
+        /*return $this->order_details()->sum(function($p){
+                    return $p['price']*$p['quantity'];
+                });*/
+
+        // return sum(['5','5']);
+
     }
+
+
+
 
 
     public function getDiscount()
+
     {
+
         // return  $this->coupon->istk ? $this->coupon->tk . ' AED' : $this->coupon->percentage. " %"  ;
+
         return  $this->discount . " %";
+
     }
+
+
 
     public function getTotal()
+
     {
+
         /* return $this->getDiscount() == "" ? $this->getSubTotal() : $this->coupon->istk ? $this->getSubTotal() - $this->coupon->tk : ""; */
+
+
 
         return ( $this->getSubTotal() - ($this->getSubTotal() * $this->discount ) / 100);
+
+
+
+        // return strval($this->getSubTotal ;
+
     }
+
+
 
     public function paid()
+
     {
+
         /* return $this->getDiscount() == "" ? $this->getSubTotal() : $this->coupon->istk ? $this->getSubTotal() - $this->coupon->tk : ""; */
 
+
+
         return ( $this->transactions->where('is_debit', '=', true)->sum(DB::raw( 'tk' )) );
+
     }
+
+
+
 
 
     public function received()
+
     {
+
         /* return $this->getDiscount() == "" ? $this->getSubTotal() : $this->coupon->istk ? $this->getSubTotal() - $this->coupon->tk : ""; */
+
+
 
         return ( $this->transactions->where('is_debit', '=', false)->sum(DB::raw( 'tk' )) );
+
     }
 
+
+
     public function balance()
+
     {
+
         /* return $this->getDiscount() == "" ? $this->getSubTotal() : $this->coupon->istk ? $this->getSubTotal() - $this->coupon->tk : ""; */
+
+
 
         return  round( (   $this->getTotal() -  ( $this->paid() - $this->received() )  ) , 2 );
 
 
 
+
+
+
+
         // $t =  (double) $this->getTotal();
-    
+
+
+
         // return $t;
+
     }
 
 
+
+
+
     public function balance_sell()
+
     {
+
         /* return $this->getDiscount() == "" ? $this->getSubTotal() : $this->coupon->istk ? $this->getSubTotal() - $this->coupon->tk : ""; */
+
+
 
         return  round( (   $this->getTotal() -  ( $this->received() - $this->paid() )  ) , 2 );
 
 
 
+
+
+
+
         // $t =  (double) $this->getTotal();
-    
+
+
+
         // return $t;
+
     }
+
+
+
+
 
 
 
--- app/Models/Orders/Order_detail.php
+++ app/Models/Orders/Order_detail.php
@@ -1,42 +1,97 @@
 <?php
 
+
+
 namespace App\Models\Orders;
+
+
 
 use Illuminate\Database\Eloquent\Model;
 
+
+
 class Order_detail extends Model
+
 {
+
     //
+
     protected $guarded = ['id'];
 
+
+
+    protected $casts = [ 'quantity' => 'integer', 'price' => 'double'  ];
+
+
+
     public function order()
+
     {
+
         return $this->belongsTo('App\Models\Orders\Order', 'order_id', 'id');
+
     }
+
+
 
     public function created_by()
+
     {
+
         return $this->belongsTo('App\User', 'created_by', 'id');
+
     }
+
+
 
     public function products()
+
     {
+
         return $this->belongsTo('App\Models\Products\Product', 'product_id', 'id');
+
     }
-    
+
+
+
     public function serial_numbers()
+
     {
+
         return $this->hasMany('App\Models\Products\Serial_number', 'order_detail_id');
+
     }
+
+
 
     public function serial_numbers_purchase()
+
     {
+
         return $this->hasMany('App\Models\Products\Serial_purchase', 'order_detail_id');
+
     }
 
+
+
+
     public function serial_numbers_sell()
+
     {
+
         return $this->hasMany('App\Models\Products\Serial_sell', 'order_detail_id');
+
     }
+
+
+    public function subtotal()
+
+    {
+
+        return $this->price * $this.quantity;
+
+    }
+
+
 
 }
